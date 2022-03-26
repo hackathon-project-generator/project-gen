5e3195493184f7928fbbd6dda080754c4534ac6a
mabusaud <mabusaud@mail.csuchico.edu>
2022-03-26 12:53:17 -0700

    added generate_project component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GenerateProjectComponent } from './generate-project/generate-project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GenerateProjectComponent
   ],
   imports: [
     BrowserModule
