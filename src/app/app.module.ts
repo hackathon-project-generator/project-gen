e4ec50c81fd9ce44f7d49b2be0322ac97d2b663b
Derrick <aragonderrick23@icloud.com>
2022-03-26 16:10:20 -0700

    time to merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { GenerateProjectComponent } from './generate-project/generate-project.component';
@@ -10,7 +11,7 @@ import { GenerateProjectComponent } from './generate-project/generate-project.co
     GenerateProjectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/generate-project/generate-project.component.ts
+++ src/app/generate-project/generate-project.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from '../project';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-generate-project',
@@ -7,20 +9,26 @@ import { Project } from '../project';
   styleUrls: ['./generate-project.component.css']
 })
 export class GenerateProjectComponent implements OnInit {
-  // project_idea:string = 'This is a project idea';
-  project: Project = {
-    type: 'Web/Mobile/CommandLine',
-    tag: 'This is a project tag...',
-    description: 'This is a long description..',
-    stack: 'this is recommended tech stack...'
-  }
-  valued:number = 0;
+
+  // project: Project = {
+  //   type: 'Web/Mobile/CommandLine',
+  //   tag: 'This is a project tag...',
+  //   description: 'This is a long description..',
+  //   stack: 'this is recommended tech stack...'
+  // }
+
+  project!: Observable<Project>;
+
+  // valued:number = 0;
   isShow:boolean = false;
+  readonly ROOT_URL = 'http://localhost:8000'
+
+  constructor(private http: HttpClient) { }
+
   GenerateProject(): void {
     this.isShow = true;
-    this.valued = Math.floor((Math.random() * 100) + 1);
+    this.project = this.http.get<Project>(this.ROOT_URL+'/project/');
   }
-  constructor() { }
 
   ngOnInit(): void {
   }
